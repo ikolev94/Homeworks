0caa2a5abe751248f50dbb148928e759a952b5f1
ikolev <ikolev94@abv.bg>
2015-07-29 10:41:54 +0300

    Bug fix and extract methods

---
+++ HQC/Refactoring/UnitTestMatrix/UnitTestRotationMatrix.cs
@@ -0,0 +1,22 @@
+﻿namespace UnitTestMatrix
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTestRotationMatrix
+    {
+        [TestMethod]
+        public void TestSize2()
+        {
+            int[,] expectedMatrix = 
+            {
+                { 1, 4 },
+                { 3, 2 }
+            };
+
+            var actualMatrix = ClockwiseRotationMatrix.WalkInMatrica.FillMartix(2);
+
+            CollectionAssert.AreEqual(expectedMatrix, actualMatrix);
+        }
+    }
+}
