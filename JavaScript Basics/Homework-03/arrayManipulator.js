3795e6f0fd984757d0f3875c39d8a2f673a23d3c
Ivan <ikolev94@yahoo.bg>
2016-01-11 20:08:47 +0200

    finish arrayManipulator

--- JavaScript Basics/Homework-03/arrayManipulator.js
+++ JavaScript Basics/Homework-03/arrayManipulator.js
@@ -1,10 +1,26 @@
 function arrayManipulator(arr) {
 
-    function compareNubs(a,b) {
+    function compareNubs(a, b) {
         return a - b;
     }
 
-    var sortedNubs =[];
+    function popular(array) {
+        if (array.length == 0) return [null, 0];
+        var n = max = 1, maxNum = array[0], pv, cv;
+
+        for (var i = 0; i < array.length; i++, pv = array[i - 1], cv = array[i]) {
+            if (pv == cv) {
+                if (++n >= max) {
+                    max = n;
+                    maxNum = cv;
+                }
+            } else n = 1;
+        }
+
+        return [maxNum];
+    }
+
+    var sortedNubs = [];
     for (var i = 0; i < arr.length; i++) {
         var obj = arr[i];
         if (!isNaN(obj)) {
@@ -13,10 +29,11 @@ function arrayManipulator(arr) {
     }
     sortedNubs.sort(compareNubs);
 
-    console.log('Min number: '+ sortedNubs[0]);
-    console.log('Min number: '+ sortedNubs[sortedNubs.length - 1]);
+    console.log('Min number: ' + sortedNubs[0]);
+    console.log('Min number: ' + sortedNubs[sortedNubs.length - 1]);
+    console.log('Most frequent number: ' + popular(sortedNubs));
     console.log(sortedNubs);
 
 }
-var test = ["Pesho", 2, "Gosho", 12, 2, "true", 9, undefined, 0, "Penka", { bunniesCount : 10}, [10, 20, 30, 40]];
+var test = ["Pesho", 2, "Gosho", 12, 2, 2, "true", 9, undefined, 0, "Penka", {bunniesCount: 10}, [10, 20, 30, 40]];
 arrayManipulator(test);
