530d76a88d2a071fe9af109e17b958d4b4736be8
Ivan <ikolev94@yahoo.bg>
2016-01-13 18:08:23 +0200

    work on repalce_A_Tag

--- JavaScript Basics/Homework-03/repalce_A_Tag.js
+++ JavaScript Basics/Homework-03/repalce_A_Tag.js
@@ -1,7 +1,12 @@
-function replaceATag (str) {
+function replaceATag(str) {
     var regex = /<a href=(.+?)>([^<]+)<\/a>/g;
-    var match = regex.exec(str);
-    while (match !== null) {
-
+    var result = [];
+    var match;
+    while (match = regex.exec(str)) {
+        result.push(match[1]);
     }
+    return result.join('');
 }
+
+var checkValue = '<p>Hello</p><a href=\'http://w3c.org\'>W3C</a>';
+console.log(replaceATag(checkValue));
