456fe145d61d73223e05e727d2acee040c945625
ivan <ikolev94@yahoo.bg>
2016-01-23 23:26:36 +0200

    done with revealTriangles

--- JavaScript Basics/Homework-02-Syntax/RevealTriangles/revealTriangles.js
+++ JavaScript Basics/Homework-02-Syntax/RevealTriangles/revealTriangles.js
@@ -1,17 +1,44 @@
 function revealTriangles(input) {
-    var output = [];
-    for (var row = 0; row < input.length; row++) {
-        output[row] = input.split('');
+    var output = [],
+        matrix = [];
+
+    for (var i = 0; i < input.length; i++) {
+        output[i] = input[i].split('');
+        matrix[i] = input[i].split('');
     }
-    console.log(output)
+
+    for (var row = 0; row < matrix.length; row++) {
+        for (var col = 0; col < matrix[row].length; col++) {
+            if (!matrix[row + 1] || !matrix[row + 1][col + 1] || !matrix[row + 1][col - 1]) {
+                continue;
+            }
+            if (matrix[row][col] === matrix[row + 1][col]
+                && matrix[row][col] === matrix[row + 1][col + 1]
+                && matrix[row][col] === matrix[row + 1][col - 1]) {
+                output[row][col] = '*';
+                output[row + 1][col] = '*';
+                output[row + 1][col + 1] = '*';
+                output[row + 1][col - 1] = '*';
+            }
+        }
+    }
+    for (var j = 0; j < output.length; j++) {
+        console.log(output[j].join(''));
+    }
 }
 
-var arr = [];
-require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-}).on('line', function (line) {
-    arr.push(line);
-}).on('close', function () {
-    revealTriangles(arr);
-});
+//revealTriangles([ 'dffdsgyefg',
+//    'ffffeyeee',
+//    'jbfffays',
+//    'dagfffdsss',
+//    'dfdfa',
+//    'dadaaadddf',
+//    'sdaaaaa',
+//    'daaaaaaasf' ]);
+//revealTriangles(
+//    [
+//        'abcdexgh',
+//        'bbbdxxxh',
+//        'abcxxxxx'
+//    ]);
+//revealTriangles([ 'aa', 'aaa', 'aaaa', 'aaaaa' ]);
