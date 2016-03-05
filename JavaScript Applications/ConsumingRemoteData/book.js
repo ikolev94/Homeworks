6a4337d3f496ebb6b5488b321aa8b620438a94aa
ivan <ikolev94@yahoo.bg>
2016-03-05 22:30:25 +0200

    finish task

--- JavaScript Applications/ConsumingRemoteData/book.js
+++ JavaScript Applications/ConsumingRemoteData/book.js
@@ -1,66 +1,81 @@
-(function () {
+$(document).ready(function () {
     "use strict";
-    var add,
+    var toAdd,
         title,
         author,
         isbn,
+        booksSelect = $('#books'),
         newTitleInput = $('#new-title'),
         newAuthorInput = $('#new-author'),
         newIsbnInput = $('#new-isbn'),
+        pop = $('#pop'),
         id = Math.floor((Math.random() * 100) + 1),
         headers = {
             'Authorization': 'Kinvey b65bc048-9cc4-48de-ad9f-9174cf2a84bf.45nQeMeS4rAV3gcF56pnd/8MMY975gEVzE+yAb0NFos=',
             'Content-Type': 'application/json'
         },
         BOOKS_URL = 'https://baas.kinvey.com/appdata/kid_W1D-ZR7pCl/books/';
-    //$('head').load('toLoad.txt style');
-    //$('body').load('toLoad.txt #wrapper');
 
-    $.ajax({
-        method: 'GET',
-        headers: headers,
-        url: BOOKS_URL,
-        success: function (data) {
-            data.forEach(function (book) {
-                sessionStorage[book.title] = book._id;
-                $('#books')
-                    .append($('<option>').attr('id', book.title).text(book.title + ' -> ' + book.author));
-            });
+    makeRequest('GET', listBooks, '');
+
+    $('#add-button').click(function () {
+        toAdd = true;
+        pop.removeClass('hidden').addClass('overlay');
+    });
+
+    $('#edit-button').click(function () {
+        if (booksSelect.val()) {
+            pop.removeClass('hidden').addClass('overlay');
+        } else {
+            alert('Select book.');
         }
-    });
-
-    $('#add-button').click(function (ev, added) {
-        add = true;
-        $('#pop').removeClass('hidden').addClass('overlay');
-    });
-
-    $('#edit-button').click(function (ev, added) {
-        //add = true;
-        newTitleInput.val()
-        $('#pop').removeClass('hidden').addClass('overlay');
     });
 
     $('#submit').click(function () {
         title = newTitleInput.val() || 'book' + id;
         author = newAuthorInput.val() || 'author' + id;
         isbn = newIsbnInput.val() || 'isbn' + id;
-        if (add) {
-            add = false;
-            $.ajax({
-                method: 'POST',
-                headers: headers,
-                url: BOOKS_URL,
-                data: JSON.stringify({'title': title, 'author': author, 'isbn': isbn}),
-                success: reset
-            })
+        if (toAdd) {
+            toAdd = false;
+            makeRequest('POST', reset, '');
         } else {
-            c
+            makeRequest('PUT', reset, sessionStorage[$('option:selected').attr('id')]);
         }
-
-        //$('#pop').removeClass('overlay').addClass('hidden');
     });
+
+    $('#cancel').click(function () {
+        pop.removeClass('overlay').addClass('hidden');
+    });
+
+    $('#delete-button').click(function () {
+        if (booksSelect.val()) {
+            makeRequest('DELETE', reset, sessionStorage[$('option:selected').attr('id')])
+        } else {
+            alert('Select book.');
+        }
+    });
+
+    function makeRequest(method, callback, id) {
+        $.ajax({
+            method: method,
+            headers: headers,
+            url: BOOKS_URL + id,
+            data: JSON.stringify({'title': title, 'author': author, 'isbn': isbn}),
+            success: callback
+        })
+    }
 
     function reset() {
         location.reload();
     }
-}());
+
+    function listBooks(data) {
+        data.forEach(function (book) {
+            sessionStorage[book.title] = book._id;
+            booksSelect.append(
+                $('<option>')
+                    .attr('id', book.title)
+                    .text(book.title + ' => ' + book.author));
+        });
+    }
+});
