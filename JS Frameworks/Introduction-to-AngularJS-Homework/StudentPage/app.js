87ddce738a33b45577c3738f8f1de85e6eb8dc02
Ivan Kolev <ikolev94@yahoo.bg>
2016-03-30 23:22:17 +0300

    add JavaScript frameworks folder

---
+++ JS Frameworks/Introduction-to-AngularJS-Homework/StudentPage/app.js
@@ -0,0 +1,13 @@
+(function () {
+    var app = angular.module('studentApp', []);
+
+    app.controller('studentController', ['$scope', function ($scope) {
+        $scope.student = {
+            "name": "Pesho",
+            "photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+            "grade": 5,
+            "school": "High School of Mathematics",
+            "teacher": "Gichka Pesheva"
+        }
+    }])
+}());
